745eabe3925c4ca1b0c52079ecdc395046045340
TONY <TONY@HOTMAIL.COM>
2019-07-05 09:50:49 +0800

    添加项目文件。

---
+++ GetTradeData/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTradeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = string.Empty;
+
+            while (true)
+            {
+                inputStr = Console.ReadLine();
+                if (inputStr == "esc")
+                    break;
+                if (inputStr == "startTrade")
+                {
+                    GetTradeData();
+                }
+            }
+
+            
+            
+        }
+
+
+
+        public static  void GetTradeData()
+        {
+            Console.WriteLine("11111");
+        }
+
+    }
+}
